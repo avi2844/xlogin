0b56603a222b2493762c2bd5bf7101a4fdb1cb24
Avinash Thakur <avinash.thakur2209@gmail.com>
2025-02-23 00:29:58 +0530

    Added login functionality

---
+++ src/components/Home.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function Home(){
+    const [loggedIn, setloggedIn] = useState(false);
+    const [isvalid, setIsValid] = useState(true);
+    const [formData, setFormData] = useState({});
+
+    const handleChange = (e) => {
+        setFormData(prevData => ({...prevData, [e.target.name] : e.target.value}));
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(formData.username === "user" && formData.password === "Password"){
+            setIsValid(true);
+            setloggedIn(true);
+        }
+        else{
+            setIsValid(false);
+            setloggedIn(false);
+        }
+    }
+
+    return(
+        <div>
+            <h1>Login Page</h1>
+            {isvalid|| <span>Invalid username or password</span>}
+            {
+                (loggedIn) ? <span>Welcome, user!</span> : 
+                <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username:</label>
+                <input type="text" name="username" placeholder="username" required onChange={handleChange}></input><br />
+                <label htmlFor="username">Password:</label>
+                <input type="password" name="password" placeholder="password" required onChange={handleChange}></input><br />
+                <button type="submit">Submit</button>
+                </form>
+            }
+        </div>
+    )
+}
+
+export default Home;
